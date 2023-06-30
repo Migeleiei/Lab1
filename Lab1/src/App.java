c77e9c7ae290005956b4c6d73d7de54084f509c7
Migeleiei <seksanfresh043@gmail.com>
2023-06-30 17:00:38 +0700

    Commit
    Final java crate new File csv

--- Lab1/src/App.java
+++ Lab1/src/App.java
@@ -1,21 +1,28 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class App {
+
+    //TODO: Change depend on Operation System
+    static String pathString = "/Users/jusuchon/Desktop/FreshJava/repo/Lab1/Lab1/src";
     public static void main(String[] args) throws Exception {
-        Scanner file = new Scanner(new File("/home/jusuchon/Downloads/Lab1/src/QS World University Rankings 2017.csv"));
+        Scanner file = new Scanner(new File(pathString+"/QS World University Rankings 2017.csv"));
+        
         ArrayList<Unidata> data = new ArrayList<Unidata>();
         
 
         file.nextLine();
         while (file.hasNext()) {
-            String read = file.nextLine();
-            // StringTokenizer st = new StringTokenizer(read, "," );
+            String read = file.nextLine();         
 
-            String pattern = "(?:^|,)(\"(?:[^\"]+)*\"|[^,]*)";
+            String pattern = "(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)";
 
             Pattern regex = Pattern.compile(pattern);
             Matcher matcher = regex.matcher(read);
@@ -41,8 +48,10 @@ public class App {
 
             data.add(unidata);
 
-        }
+        } 
 
+      
+     
 
         for(Unidata uni: data){
            System.out.println("                 University");
@@ -57,9 +66,43 @@ public class App {
            System.out.println("      "+"Country : "+uni.getCountry()+ " City : " +uni.getCity() + " Region : "+uni.getRegion());
            System.out.println("==============================================");
            System.out.println("");
-
               
         }
+
+        String cmuUniversity = "2017,3,Chiangmai University,90,https://www.cmu.ac.th/,Thailand,Chiamgmai,Asia,https://www.cmu.ac.th/th/cmu/symbol";
+        createNewFile(data,cmuUniversity);
+    }
+
+
+    private static void createNewFile(ArrayList<Unidata> uniList,String CMUDetail){
+        String fileName = "QS_World University Rankings 2017_out.csv";
+
+        try {
+            
+            FileWriter nFile = new FileWriter(fileName);
+            StringBuilder sb = new StringBuilder();
+            
+            for(Unidata unidata: uniList){
+                String uniString = unidata.getYear() +","+ unidata.getRank() + "," + unidata.getUniversity() + "," + unidata.getScore() + "," + unidata.getLink() + 
+                "," + unidata.getCountry() + "," + unidata.getCity() + "," + unidata.getRegion() + "," + unidata.getLogo();
+                  
+                sb.append(uniString);
+                sb.append(System.lineSeparator());
+                
+            }
+
+            System.out.println(sb);
+            sb.append(CMUDetail);
+            sb.append(System.lineSeparator());
+            nFile.write(sb.toString());
+            nFile.close();       
+          
+
+        } catch (Exception e) {
+            System.out.println("Create File Error : "+e.toString());
+        }
+
     }
 
 }
+
